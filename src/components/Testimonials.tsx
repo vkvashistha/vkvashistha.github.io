d32da7ac1992e55bd23eb01e29ee0f46960254e7
vivek-vashistha <vivek.v@turing.com>
2025-12-10 16:10:47 +0530

    testimonials updated

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,20 +7,34 @@ import 'swiper/css/pagination'
 
 import testimonialRanjeeta from '../assets/img/testimonial-ranjeeta.png'
 import testimonialAnurag from '../assets/img/testimonial-anurag.png'
+import testimonialKapil from '../assets/img/testimonial-kapil.jpeg'
+import testimonialAnil from '../assets/img/testimonial-anil.jpeg'
 
 const testimonials = [
   {
     image: testimonialRanjeeta,
     name: 'Ranjeeta Nanda',
-    title: 'Engineering Manager',
+    title: 'CEO, Park Circle Technologies',
     text: 'Vivek played a crucial role in our year-long Android app project, successfully extending a legacy tablet application from supporting a single use-case to multiple use-cases. He quickly grasped and revamped complex, outdated code, resolving bugs, optimizing performance, and upgrading critical components. His exceptional skills in design, architecture, and coding were pivotal to the project\'s success.',
   },
   {
     image: testimonialAnurag,
-    name: 'Anurag Jain',
-    title: 'Content Creator & Entrepreneur',
+    name: 'Anurag Wadehra',
+    title: 'Growth Coach for Tech Founders & CEOs',
     text: 'Vivek streamlined my subscriber management by integrating multiple platforms into ConvertKit, automating tagging and list management, and creating an effective email campaign. Their clear documentation enables me to handle everything independently. Highly recommended for their efficient and impactful solutions!',
   },
+  {
+    image: testimonialKapil,
+    name: 'Kapil Saxena',
+    title: 'Director of Engineering, CaaStle',
+    text: 'Vivek is very thorough and brings with him vast technical knowledge on the subject. Once a problem is mentioned to him, he comes up with alternatives to approach the problem with cost and resource efficient way. He is a great thinker. Not only his design and architecture skills but implementation and code quality skills are superb. He can be relied upon to deliver high optimised, quality code.'
+  },
+  {
+    image: testimonialAnil,
+    name: 'Anil Bains',
+    title: 'CEO, Founder, AttryB',
+    text: 'Vivek through his structured thinking and technical expertise helped in streamlining the software development process at very early stage of the company.'
+  }
 ]
 
 export default function Testimonials() {
